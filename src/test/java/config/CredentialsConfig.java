5cf904679fd8639b189b70aa9fbf91ba348f823c
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-05-15 02:59:53 +0700

    hw

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -2,7 +2,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({"classpath:credentials.properties"})
-public interface CredentialsConfig extends Config {
+public interface CredentialsConfig extends Config{
 
     @Key("user")
     String user();
